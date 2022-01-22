d202bccdce4d98ac02864b5c722549f6cd432268
spchee <53122886+spchee@users.noreply.github.com>
2022-01-22 18:41:00 +0000

    Initial Commit for requests

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from serializers import ProductSerializer
+from .models import Product
+
+class GetProduct(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+
+    def get(self, pk):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
